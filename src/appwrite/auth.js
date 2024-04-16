ea9ee5a10556d44c387fc1f81264b1286ed54f6f
rupambora31 <iamrupam99@gmail.com>
2024-04-16 12:22:39 +0530

    add AuthService class

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,1 +1,63 @@
- 
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async register({ email, password, name, homeTown }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name,
+        homeTown
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return this.account.get;
+    } catch (error) {
+      console.log("Apprite service :: getCurrentUser :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Apprite service :: logout() :: ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
