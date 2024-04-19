00cddb0e71417c7ede60e4107504290959fdcc49
rupambora31 <iamrupam99@gmail.com>
2024-04-19 09:13:58 +0530

    install and config shadcn

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,15 +1,16 @@
+
 const conf = {
-  appwriteUrl: String(process.env.VITE_APP_APPWRITE_URL),
+  appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
 
-  appwriteProjectId: String(process.env.VITE_APP_APPWRITE_PROJECT_ID),
+  appwriteProjectId: String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
 
-  appwriteDatabaseId: String(process.env.VITE_APP_APPWRITE_DATABASE_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
 
   appwritePostCollectionId: String(
-    process.env.VITE_APP_APPWRITE_POST_COLLECTION_ID
+    import.meta.env.VITE_APP_APPWRITE_POST_COLLECTION_ID
   ),
 
-  appwriteBucketId: String(process.env.VITE_APP_APPWRITE_BUCKET_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
 };
 
 export default conf;
